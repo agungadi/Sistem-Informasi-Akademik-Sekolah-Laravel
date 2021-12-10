b4327542517c33d031811734f1e4a896293a31d9
Firman Nugroho <56217126+fmanid@users.noreply.github.com>
2021-12-10 12:39:08 +0700

    update 7-createdatauser add edit uname, pw, email

--- cypress/integration/3-tugas/7-createdatauser.spec.js
+++ cypress/integration/3-tugas/7-createdatauser.spec.js
@@ -1,3 +1,4 @@
+
 describe("Login Admin", function () {
     beforeEach(function () {
     cy.fixture("admin").then(function (admin) {
@@ -13,11 +14,13 @@ describe("Login Admin", function () {
     });
 });
 
-it("Tambah Data User", function () {
+it.only("Tambah Data User", function () {
     cy.url().should('include','/');
 
     cy.get("#liMasterData").click();
+    cy.wait(150);
     cy.get('#DataUser').click();
+    cy.wait(1500);
     cy.url().should('include','/user');
     cy.contains("Data User").should("be.visible");
     cy.get("body > div > div.content-wrapper > section > div > div > div.col-md-12 > div > div.card-header > h3 > button").click()
@@ -35,8 +38,12 @@ it("Tambah Data User", function () {
     cy.contains('Log Out').click({force:true})
     cy.url().should('include','/');
 
-    cy.visit("/login");
-      cy.url().should('include','/login');
+});
+});
+
+describe("Test Login dan Edit user", function () {
+    it.only("Edit User", function () {
+      cy.visit("/login");;
       cy.get("#email").type("yudisolo@gmail.com");
       cy.wait(1500);
       cy.get("#password").type("12345678");
@@ -45,7 +52,47 @@ it("Tambah Data User", function () {
       cy.get('#btnGroupDrop1').click();
       cy.contains('My Profile').click({force:true})
       cy.contains("yudisolo").should("be.visible");
-   
+
+      cy.get('body > div > div.content-wrapper > section > div > div > div > div > div.col-5 > div:nth-child(1) > div > a').click();
+      cy.get("#name").clear();
+      cy.get("#name").type("yudijember");
+      cy.get('body > div > div.content-wrapper > section > div > div > div > div > form > div.card-footer > button').click();
+      cy.wait(5000);
+      cy.contains("yudijember").should("be.visible");
+
+      cy.get('body > div > div.content-wrapper > section > div > div > div > div > div.col-5 > div:nth-child(2) > div.card-body > table > tbody > tr:nth-child(1) > td:nth-child(3) > a').click();
+      cy.get("#email").type("yudijember@gmail.com");
+      cy.get('body > div > div.content-wrapper > section > div > div > div > div > form > div.card-footer > button').click();
+      cy.get('#back').click();
+
+      cy.get('body > div > div.content-wrapper > section > div > div > div > div > div.col-5 > div:nth-child(2) > div.card-body > table > tbody > tr:nth-child(2) > td:nth-child(3) > a').click();
+      cy.get("#password-old").type("12345678");
+      cy.get("#password").type("123456789");
+      cy.get("#password-confirm").type("123456789");
+      cy.get('body > div > div.content-wrapper > section > div > div > div > div > form > div.card-footer > button').click();
+      cy.get('#back').click();
+      
+    
+      cy.get('#btnGroupDrop1').click()
+      cy.contains('Log Out').click({force:true})
+      cy.url().should('include','/');
+      
+});
+});
+
+describe("Test Login updated user", function () {
+    it.only("Edit User", function () {
+      cy.visit("/login");;
+      cy.get("#email").type("yudijember@gmail.com");
+      cy.wait(1500);
+      cy.get("#password").type("123456789");
+      cy.get("#btn-login").click();
+      cy.contains("Dashboard").should("be.visible");
+      cy.get('#btnGroupDrop1').click();
+      cy.contains('My Profile').click({force:true})
+      cy.contains("yudijember").should("be.visible");
+
+      
 });
 });
 
